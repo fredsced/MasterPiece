6132d2154da83399059afd8259c2fe162f8c68d2
Frédéric Descloux <frederic.descloux@gmail.com>
2020-01-24 12:51:36 +0100

    initial commit

---
+++ back/src/test/java/fr/formation/itschool/masterpiece/MasterpieceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.itschool.masterpiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterpieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
