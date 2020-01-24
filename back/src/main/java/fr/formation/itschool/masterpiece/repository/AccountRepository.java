c7d3d0c24059ebcd027785a47d06ecbadeffa140
Frédéric Descloux <frederic.descloux@gmail.com>
2020-01-24 19:21:00 +0100

    clean back

--- back/src/main/java/fr/formation/itschool/masterpiece/repository/AccountRepository.java
+++ back/src/main/java/fr/formation/itschool/masterpiece/repository/AccountRepository.java
@@ -3,7 +3,6 @@ package fr.formation.itschool.masterpiece.repository;
 import fr.formation.itschool.masterpiece.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository <Account, Long> {
