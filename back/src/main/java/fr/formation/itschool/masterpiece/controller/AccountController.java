9d754a2e3a1bda0d8c9f3b65eed73e51d45a1113
Frédéric Descloux <frederic.descloux@gmail.com>
2020-01-29 22:24:34 +0100

    reformat with google format java

--- back/src/main/java/fr/formation/itschool/masterpiece/controller/AccountController.java
+++ back/src/main/java/fr/formation/itschool/masterpiece/controller/AccountController.java
@@ -4,28 +4,30 @@ import fr.formation.itschool.masterpiece.dto.AccountDto;
 import fr.formation.itschool.masterpiece.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value="/api")
-public class AccountController {
+@RequestMapping(value = "/api")
+class AccountController {
 
-    private final AccountService service;
+  private final AccountService service;
 
-    protected AccountController(AccountService service){
-        this.service = service;
+  protected AccountController(AccountService service) {
+    this.service = service;
+  }
+
+  @PostMapping("/account")
+  public ResponseEntity<String> newAccount(@Valid @RequestBody AccountDto accountdto) {
+    if (service.createAccount(accountdto)) {
+      return new ResponseEntity<>("Account created", HttpStatus.CREATED);
     }
-
-    @PostMapping("/account")
-    public ResponseEntity<String> newAccount(@Valid @RequestBody AccountDto accountdto)  {
-        if (service.createAccount(accountdto)) {
-            return new ResponseEntity<>("Account created", HttpStatus.CREATED);
-        }
-        else {
-            return new ResponseEntity<>("Email already presents", HttpStatus.CONFLICT);
-        }
-    }
+    return new ResponseEntity<>("Email already presents", HttpStatus.CONFLICT);
+  }
 }
