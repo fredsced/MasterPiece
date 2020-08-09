141d09ecf72ebb2f850c829b9204bf3c2813e209
Frédéric Descloux <frederic.descloux@gmail.com>
2020-08-09 21:10:32 +0200

    add .env

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
-const sitename = 'My ConformityPal';
-
 const Footer = () => {
     return (
         <Box mt={5}>
             <Typography variant="body2" color="textSecondary" align="center">
-                {`Copyright © ${sitename} ${new Date().getFullYear()}.`}
+                {`Copyright © ${process.env.REACT_APP_WEBSITE_NAME} ${new Date().getFullYear()}.`}
             </Typography>
         </Box>
     );
