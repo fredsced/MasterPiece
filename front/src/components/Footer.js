840e000dd58a7a8dae3dfd778d5366d0a4a05209
Frédéric Descloux <frederic.descloux@gmail.com>
2020-08-09 18:45:11 +0200

    gather sign in and sign uu in one component sign

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+
 const sitename = 'My ConformityPal';
 
-function Footer() {
+const Footer = () => {
     return (
         <Box mt={5}>
             <Typography variant="body2" color="textSecondary" align="center">
---
+++ front/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography'
+const Header = () => {
+    return (
+        <header>
+            <Typography component="h1" variant="h1">
+                My Comformity Pal
+            </Typography>
+        </header>
+    )
+}
+
+export default Header;
