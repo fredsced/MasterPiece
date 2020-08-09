60ef7ad71c42315f636313f49862685edbccd27d
Frédéric Descloux <frederic.descloux@gmail.com>
2020-08-09 18:37:46 +0200

    response controller for field exception

---
+++ back/src/main/java/fr/formation/itschool/masterpiece/controllers/ControllerAdvice.java
@@ -0,0 +1,34 @@
+package fr.formation.itschool.masterpiece.controllers;
+
+import fr.formation.itschool.masterpiece.errors.ValidationError;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ControllerAdvice extends ResponseEntityExceptionHandler {
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest webRequest) {
+    BindingResult result = ex.getBindingResult();
+    List<FieldError> fieldErrors = result.getFieldErrors();
+
+    List<ValidationError> validationErrors =
+        fieldErrors.stream()
+            .map(error -> new ValidationError(error.getCode(), error.getField()))
+            .collect(Collectors.toList());
+    return super.handleExceptionInternal(ex, validationErrors, headers, status, webRequest);
+  }
+}
