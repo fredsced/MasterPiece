69f270c8530edd449b98eac0325c2b6199d4582c
Frédéric Descloux <frederic.descloux@gmail.com>
2020-10-11 11:42:34 +0200

    profile page add link to login

--- front/src/components/UserProfile.jsx
+++ front/src/components/UserProfile.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link as RouterLink } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   main: {
@@ -14,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    padding: theme.spacing(6, 3),
+    padding: theme.spacing(6, 3, 2),
   },
 }));
 export default function Userprofile(props) {
@@ -29,9 +32,20 @@ export default function Userprofile(props) {
             {`Hello ${user.userEmail}`}
           </Typography>
         ) : (
-          <Typography component='p' variant='body1'>
-            Vous n'êtes pas connecté
-          </Typography>
+          <Grid container spacing={2} justify='center'>
+            <Grid item={12}>
+              <Typography component='h1' variant='h4'>
+                Vous n'êtes pas connecté
+              </Typography>
+            </Grid>
+            <Grid container spacing={2} justify='flex-end'>
+              <Grid item>
+                <Link component={RouterLink} to='/login' variant='body2'>
+                  Connectez vous
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
         )}
       </Paper>
     </Container>
