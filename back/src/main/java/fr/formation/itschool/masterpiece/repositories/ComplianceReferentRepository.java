add964a2a4236b745a00446d66cbee3be66c59df
Frédéric Descloux <frederic.descloux@gmail.com>
2020-11-19 11:23:19 +0100

    change native sql request by jpql

--- back/src/main/java/fr/formation/itschool/masterpiece/repositories/ComplianceReferentRepository.java
+++ back/src/main/java/fr/formation/itschool/masterpiece/repositories/ComplianceReferentRepository.java
@@ -11,14 +11,9 @@ import java.util.List;
 public interface ComplianceReferentRepository extends JpaRepository<ComplianceReferent, Long> {
 
   @Query(
-      value =
-          "select co.name, co.firstname, cn.iso country, ou.code, r.code risk from compliance_referents cr \n"
-              + "join collaborators co on co.id = cr.collaborator_id \n"
-              + "join organisation_units ou on ou.id = co.organisation_unit_id \n"
-              + "join countries cn on cn.id = co.country_id \n"
-              + "join risks r on r.id = cr.risk_id \n"
-              + "where r.code=:input and ou.id like :buId and cn.id like :countryId",
-      nativeQuery = true)
-  List<LcoViewDto> findLcoByRisk(
-      @Param("input") String riskCode, @Param("countryId") Long countryId, @Param("buId") Long buId);
+      "SELECT new fr.formation.itschool.masterpiece.dtos.LcoViewDto(c.name, c.firstName, co.iso, ou.code, r.code) FROM ComplianceReferent cr join cr.collaborator c join c.country co join c.organisationUnit ou join cr.risk r WHERE r.code=:input AND ou.id=:buId AND co.id=:countryId")
+  List<LcoViewDto> findMyComplianceReferentByRisk(
+      @Param("input") String riskCode,
+      @Param("countryId") Long countryId,
+      @Param("buId") Long buId);
 }
