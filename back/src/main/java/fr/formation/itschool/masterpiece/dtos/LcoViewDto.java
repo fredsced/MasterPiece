93fd899f2228cb4575387c720f9bcf502aea2a1c
Frédéric Descloux <frederic.descloux@gmail.com>
2020-11-19 11:24:13 +0100

    change LcoView interface by a class cause constructor needed for query projection

--- back/src/main/java/fr/formation/itschool/masterpiece/dtos/LcoViewDto.java
+++ back/src/main/java/fr/formation/itschool/masterpiece/dtos/LcoViewDto.java
@@ -1,9 +1,58 @@
 package fr.formation.itschool.masterpiece.dtos;
 
-public interface LcoViewDto {
-  String getName();
-  String getFirstname();
-  String getCountry();
-  String getCode();
-  String getRisk();
+public class LcoViewDto {
+
+  private String name;
+  private String firstName;
+  private String country;
+  private String buCode;
+  private String riskCode;
+
+  public LcoViewDto(String name, String firstName, String country, String buCode, String riskCode) {
+    this.name = name;
+    this.firstName = firstName;
+    this.country = country;
+    this.buCode = buCode;
+    this.riskCode = riskCode;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getBuCode() {
+    return buCode;
+  }
+
+  public void setBuCode(String buCode) {
+    this.buCode = buCode;
+  }
+
+  public String getRiskCode() {
+    return riskCode;
+  }
+
+  public void setRiskCode(String riskCode) {
+    this.riskCode = riskCode;
+  }
 }
