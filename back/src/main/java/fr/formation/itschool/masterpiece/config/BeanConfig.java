c921ff5614e1d700fbee693660030b188d2a5751
Frédéric Descloux <frederic.descloux@gmail.com>
2021-03-20 23:05:39 +0100

    [Document] java Doc for Beans in BenConfig

--- back/src/main/java/fr/formation/itschool/masterpiece/config/BeanConfig.java
+++ back/src/main/java/fr/formation/itschool/masterpiece/config/BeanConfig.java
@@ -21,30 +21,44 @@ public class BeanConfig {
     return new BCryptPasswordEncoder();
   }
 
-  /**
-   * The modelMapper for application. Used to make the mapping between dtos and entities.
-   *
-   * @return a {@code ModelMapper} instance
-   */
-  @Bean
-  protected ModelMapper modelMapper() {
-    ModelMapper modelMapper = new ModelMapper();
-    modelMapper
-      .getConfiguration()
-      .setFieldMatchingEnabled(true)
-      .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
-      .setMatchingStrategy(MatchingStrategies.STANDARD);
-    return modelMapper;
-  }
-
-  /**
-   * The cacheManager for application. Used to provided referential data without accessing DB after the first time.
-   *
-   * @return a {@code ConcurrentMapCacheManager} instance
-   */
-  @Bean
-  protected CacheManager cacheManager() {
-    return new ConcurrentMapCacheManager("countries", "organisationUnits", "risks");
+    /**
+     * The modelMapper for application. Used to make the mapping between dtos and entities.
+     *
+     * @return a {@code ModelMapper} instance
+     */
+    @Bean
+    protected ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper
+                .getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+    }
+    /**
+     * The objectMapper for application. Used to make the mapping between json and dtos.
+     *
+     * @return a {@code ObjectMapper} instance
+     */
+    /*@Bean
+    protected ObjectMapper objectMapper() {
+        ObjectMapper mapper = Jackson2ObjectMapperBuilder.json().build();
+        mapper.findAndRegisterModules();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        mapper.setVisibility(
+          mapper.getSerializationConfig().getDefaultVisibilityChecker()
+            .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+            .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+            .withIsGetterVisibility(JsonAutoDetect.Visibility.NONE)
+            .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
+        return mapper;
+    }
+*/
+    @Bean
+    public CacheManager cacheManager(){
+        return new ConcurrentMapCacheManager("countries","organisationUnits","risks");
 
   }
 
