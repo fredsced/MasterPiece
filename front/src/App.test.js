c8ca05e39a20c6276e5dde58c86ad37237e811cc
Frédéric Descloux <frederic.descloux@gmail.com>
2021-04-23 18:37:17 +0200

    [Update][package json]

--- front/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('control title in page', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText('myCompliancePal');
-  expect(linkElement).toBeInTheDocument();
-});
